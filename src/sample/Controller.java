fbd3c48b974c11ae783d0bf1be8cfdc84bcfbdf8
himdavid <exta02@gmail.com>
2017-02-01 12:12:25 -0800

    The UI is done. Adding ConfigData, CreateTestFiles and Time.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
-
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,6 +19,8 @@ public class Controller implements Initializable {
 
     @FXML
     private Label delimiterLabel;
+    @FXML
+    private Label step2Label;
     @FXML
     private ComboBox<String> delimiter;
     @FXML
@@ -49,6 +50,8 @@ public class Controller implements Initializable {
     @FXML
     private Label step4Label;
     @FXML
+    private Label step5Label;
+    @FXML
     private Label fileNameFormatStatusLabel;
     @FXML
     private Label fileNameFormatName;
@@ -70,27 +73,48 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         delimiter.setItems(delimiterList);
-        delimiterCharacter.setVisible(false);
-        baselineFilePath.setVisible(false);
-        configFilePath.setVisible(false);
+        delimiterCharacter.setDisable(true);
+        step2Label.setDisable(true);
+        step2Label.setText("---");
+        step3Label.setText("---");
+        step3Label.setDisable(true);
+        baselineFileButton.setDisable(true);
+        step4Label.setDisable(true);
+        step4Label.setText("---");
+        step5Label.setDisable(true);
+        step5Label.setText("---");
+        fileNameFormatText.setDisable(true);
         magicButton.setDisable(true);
     }
 
     public void delimiterChange(ActionEvent event) {
         String delimiterType = delimiter.getValue();
         if(delimiterType.equals("Delimited")) {
-            delimiterCharacter.setVisible(true);
-            configFileButton.setDisable(true);
-            step3Label.setDisable(true);
-            step3Label.setText("NA");
-            step4Label.setText("Step 3:");
-        } else {
-            delimiterCharacter.setVisible(false);
-            delimiterStatusLabel.setTextFill(Color.GREEN);
-            configFileButton.setDisable(false);
-            step3Label.setDisable(false);
+            step2Label.setDisable(false);
+            step2Label.setText("Step 2:");
             step3Label.setText("Step 3:");
-            step4Label.setText("Step 4:");
+            step4Label.setText("---");
+            step5Label.setText("Step 4:");
+            delimiterCharacter.setDisable(false);
+            step4Label.setDisable(true);
+            configFileButton.setDisable(true);
+        } else {
+            step2Label.setDisable(true);
+            step2Label.setText("---");
+            delimiterCharacter.setDisable(true);
+            delimiterStatusLabel.setTextFill(Color.GREEN);
+            baselineFileButton.setDisable(false);
+            step3Label.setDisable(false);
+            step3Label.setText("Step 2:");
+            step4Label.setText("Step 3:");
+            step5Label.setText("Step 4:");
+            if(!baselineFilePath.getText().equals("")) {
+                step3Label.setDisable(false);
+                step3Label.setText("Step 2:");
+                step4Label.setDisable(false);
+                step4Label.setText("Step 3:");
+                configFileButton.setDisable(false);
+            }
         }
 
         delimiterLabel.setText(delimiterType);
@@ -103,47 +127,74 @@ public class Controller implements Initializable {
         if(!delimiterCharacterText.trim().equals("")) {
             delimiterLabel.setText(delimiter.getValue() + ": " + delimiterCharacterText);
             delimiterStatusLabel.setTextFill(Color.GREEN);
+            step3Label.setDisable(false);
+            baselineFileButton.setDisable(false);
             enableMagicButton();
         } else {
             delimiterStatusLabel.setTextFill(Color.RED);
         }
-
-
     }
 
     public void uploadBaselineFileAction(ActionEvent event) {
-        FileChooser fileChooserBaseline = new FileChooser();
-        fileChooserBaseline.setTitle("Select your baseline file");
-        fileChooserBaseline.setInitialDirectory(new File(System.getProperty("user.home")));
+        FileChooser fileChooserBaseline;
 
-        File baselineFile = fileChooserBaseline.showOpenDialog(baselineFileButton.getScene().getWindow());
-        String filePath = baselineFile.getAbsolutePath();
-        String fileName = baselineFile.getName();
-        baselineFilePath.setVisible(true);
-        baselineFilePath.setText(filePath);
-        baselineFileName.setText(fileName);
-        baselineStatusLabel.setTextFill(Color.GREEN);
+        try {
+            fileChooserBaseline = new FileChooser();
+            fileChooserBaseline.setTitle("Select your baseline file");
+            fileChooserBaseline.setInitialDirectory(new File(System.getProperty("user.home")));
+            File baselineFile = fileChooserBaseline.showOpenDialog(baselineFileButton.getScene().getWindow());
+            String filePath = baselineFile.getAbsolutePath();
+            String fileName = baselineFile.getName();
+            baselineFilePath.setText(filePath);
+            baselineFileName.setText(fileName);
+
+            baselineStatusLabel.setTextFill(Color.GREEN);
+
+            if(delimiter.getValue().equals("Fixed width")) {
+                step4Label.setDisable(false);
+                configFileButton.setDisable(false);
+                fileNameFormatText.setDisable(false);
+            } else {
+                step5Label.setDisable(false);
+                fileNameFormatText.setDisable(false);
+            }
+
+
+        } catch(NullPointerException ex) {
+            System.out.println("Please select a baseline file to upload");
+        }
         enableMagicButton();
     }
 
     public void uploadConfigFileAction(ActionEvent event) {
-        FileChooser fileChooserConfig = new FileChooser();
-        fileChooserConfig.setTitle("Select your config file");
-        fileChooserConfig.setInitialDirectory(new File(System.getProperty("user.home")));
 
-        File configFile = fileChooserConfig.showOpenDialog(configFileButton.getScene().getWindow());
-        String filePath = configFile.getAbsolutePath();
-        String fileName = configFile.getName();
-        configFilePath.setVisible(true);
-        configFilePath.setText(filePath);
-        configFileName.setText(fileName);
-        configFileStatusLabel.setTextFill(Color.GREEN);
+        try {
+            FileChooser fileChooserConfig = new FileChooser();
+            fileChooserConfig.setTitle("Select your config file");
+            fileChooserConfig.setInitialDirectory(new File(System.getProperty("user.home")));
+
+            File configFile = fileChooserConfig.showOpenDialog(configFileButton.getScene().getWindow());
+            String filePath = configFile.getAbsolutePath();
+            String fileName = configFile.getName();
+            configFilePath.setText(filePath);
+            configFileName.setText(fileName);
+            step5Label.setDisable(false);
+            configFileStatusLabel.setTextFill(Color.GREEN);
+        } catch(NullPointerException ex) {
+            System.out.println("Please select a config file to upload");
+        }
         enableMagicButton();
     }
 
     public void fileNameFormatChange(ActionEvent event) {
-        fileNameFormatName.setText(fileNameFormatText.getText());
-        fileNameFormatStatusLabel.setTextFill(Color.GREEN);
+        String fileNameText = fileNameFormatText.getText();
+        fileNameFormatName.setText(fileNameText);
+
+        if(fileNameText.trim().equals("")) {
+            fileNameFormatStatusLabel.setTextFill(Color.RED);
+        } else {
+            fileNameFormatStatusLabel.setTextFill(Color.GREEN);
+        }
         enableMagicButton();
     }
 
