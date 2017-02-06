297f1b41378dfb68255dfff4ece34dc94e477dcf
himdavid <exta02@gmail.com>
2017-02-05 21:10:03 -0800

    committing changes for a working createoutput and createoutputfilxedwidth.

--- src/sample/CreateTestFiles.java
+++ src/sample/CreateTestFiles.java
@@ -117,81 +117,7 @@ public class CreateTestFiles {
         return map;
     }
 
-    public File createOutputFixedWidthCopy(String testCaseFilePath, String testDataFilePath, String configFilePath, String fileNameFormat, boolean header, boolean footer) throws IOException {
-        ArrayList testCasesLists = new ArrayList();
-        File testFile = null;
-        BufferedReader br = new BufferedReader(new FileReader(testCaseFilePath));
-        String content = null;
-        //String filePath = testCaseFilePath.substring(0, testCaseFilePath.lastIndexOf("/")) + "/";
-        String filePath = testCaseFilePath.substring(0, testCaseFilePath.lastIndexOf("\\")) + "\\";
-
-        while((content = br.readLine()) != null) {
-            if(content.contains(",")) {
-                testCasesLists = new ArrayList(Arrays.asList(content.split(",")));
-            } else {
-                testCasesLists.add(content);
-            }
-
-            HashMap map = this.parseFieldsFixedWidth(testDataFilePath, configFilePath, header, footer);
-            HashMap updatedMap = this.updateFieldValueFixedWidth(map, testCasesLists);
-            String fileName = filePath + fileNameFormat.replace("[FieldName]", content.replace("\"", ""));
-            fileName = fileName.substring(0, Math.min(fileName.length(), 260));
-            testFile = new File(fileName);
-            File tempFileValue = new File(filePath + "tempValue.dat");
-            FileOutputStream fopsFinalFile = new FileOutputStream(testFile);
-            FileOutputStream fopsValue = new FileOutputStream(tempFileValue);
-            System.out.println(testFile.getAbsolutePath());
-            System.out.println("file length is: " + testFile.getAbsolutePath().length());
-            Iterator brValue = updatedMap.entrySet().iterator();
-
-            String fopsFooter;
-            while(brValue.hasNext()) {
-                Entry copyValue = (Entry)brValue.next();
-                String tempFileFooter = (String)copyValue.getKey();
-                fopsFooter = ((ConfigData)copyValue.getValue()).getValue();
-                fopsValue.write(fopsFooter.getBytes());
-                System.out.println("Field:" + tempFileFooter + " \n Value:" + fopsFooter);
-            }
-
-            if(header) {
-                File brValue1 = new File(filePath + "tempHeader.dat");
-                FileOutputStream copyValue1 = new FileOutputStream(brValue1);
-                copyValue1.write(this.getHeader(testDataFilePath).getBytes());
-                BufferedReader tempFileFooter1 = new BufferedReader(new FileReader(brValue1));
-                fopsFooter = tempFileFooter1.readLine() + "\n";
-                fopsFinalFile.write(fopsFooter.getBytes());
-                copyValue1.close();
-                tempFileFooter1.close();
-                brValue1.delete();
-            }
-
-            fopsValue.close();
-            BufferedReader brValue2 = new BufferedReader(new FileReader(tempFileValue));
-            String copyValue2 = brValue2.readLine();
-            fopsFinalFile.write(copyValue2.getBytes());
-            brValue2.close();
-            tempFileValue.delete();
-            if(footer) {
-                File tempFileFooter2 = new File(filePath + "tempFooter.dat");
-                FileOutputStream fopsFooter1 = new FileOutputStream(tempFileFooter2);
-                fopsFooter1.write(this.getFooter(testDataFilePath, header).getBytes());
-                BufferedReader brFooter = new BufferedReader(new FileReader(tempFileFooter2));
-                String copyFooter = "\n" + brFooter.readLine();
-                fopsFinalFile.write(copyFooter.getBytes());
-                fopsFooter1.close();
-                brFooter.close();
-                tempFileFooter2.delete();
-            }
-
-            testCasesLists.clear();
-            fopsFinalFile.close();
-        }
-
-        br.close();
-        return testFile;
-    }
-
-    public File createOutputFixedWidth(String testCaseFilePath, String testDataFilePath, String configFilePath, String fileNameFormat, boolean header, boolean footer) throws IOException {
+     public File createOutputFixedWidth(String testCaseFilePath, String testDataFilePath, String configFilePath, String fileNameFormat, boolean header, boolean footer) throws IOException {
         ArrayList testCasesLists = new ArrayList();
         File testFile = null;
         BufferedReader br = new BufferedReader(new FileReader(testCaseFilePath));
@@ -298,8 +224,11 @@ public class CreateTestFiles {
                 tempFile = new File(filePath +"temp.dat");
                 fops2 = new FileOutputStream(tempFile);
             } else if(multipleFilesFlag.equals("Multiple")) {
-                testFile = new File(filePath + fileNameFormat.replace("[FieldName]", "MultipleRecords"));
+                testFile = new File(fileName);
+                //testFile = new File(filePath + fileNameFormat.replace("[FieldName]", "MultipleRecords"));
                 fops1 = new FileOutputStream(testFile);
+                tempFile = new File(filePath +"temp.dat");
+                fops2 = new FileOutputStream(tempFile);
             }
 
             //System.out.println(testFile.getAbsolutePath());
@@ -322,7 +251,9 @@ public class CreateTestFiles {
                 }
             }
             fops2.write("\n".getBytes());
-            createHeader = false;
+            if(multipleFilesFlag.equals("Single")) {
+                createHeader = false;
+            }
 
             testCasesLists.clear();
             if(multipleFilesFlag.equals("Multiple")) {
@@ -347,66 +278,6 @@ public class CreateTestFiles {
             var25.close();
             fops1.close();
             //tempFile.delete();
-        }
-
-        br.close();
-        return testFileList;
-    }
-
-    public ArrayList<String> createOutputCopy(String testCaseFilePath, String testDataFilePath, String delimiter, String fileNameFormat, String multipleFilesFlag) throws IOException {
-        ArrayList testCasesLists = null;
-        File testFile = null;
-        ArrayList testFileList = new ArrayList();
-        BufferedReader br = new BufferedReader(new FileReader(testCaseFilePath));
-        String filePath = testCaseFilePath.substring(0, testCaseFilePath.lastIndexOf("\\")) + "\\";
-
-        String content;
-        while((content = br.readLine()) != null) {
-            if(content.contains(",")) {
-                testCasesLists = new ArrayList(Arrays.asList(content.split(",")));
-            } else {
-                testCasesLists.add(content);
-            }
-
-            HashMap map = this.parseFieldsDelimited(testDataFilePath, delimiter);
-            HashMap updatedMap = this.updateFieldValue(map, testCasesLists);
-            String fileName = filePath + fileNameFormat.replace("[FieldName]", content.replace("\"", ""));
-            fileName = fileName.substring(0, Math.min(fileName.length(), 260));
-            testFileList.add(fileName);
-            if(multipleFilesFlag.equals("Single") && testFile == null) {
-                testFile = new File(fileName);
-            } else {
-                testFile = new File(filePath + fileNameFormat.replace("[FieldName]", "MultipleRecords"));
-            }
-
-            File tempFile = new File("temp.dat");
-            FileOutputStream fops1 = new FileOutputStream(testFile);
-            FileOutputStream fops2 = new FileOutputStream(tempFile);
-            System.out.println(testFile.getAbsolutePath());
-            System.out.println("file length is: " + testFile.getAbsolutePath().length());
-            int setSize = updatedMap.entrySet().size();
-            int count = 1;
-
-            for(Iterator br2 = updatedMap.entrySet().iterator(); br2.hasNext(); ++count) {
-                Entry copyString = (Entry)br2.next();
-                String field = (String)copyString.getKey();
-                String value = (String)copyString.getValue();
-                fops1.write(field.getBytes());
-                fops2.write(value.getBytes());
-                if(count < setSize) {
-                    fops1.write(delimiter.getBytes());
-                    fops2.write(delimiter.getBytes());
-                }
-            }
-
-            testCasesLists.clear();
-            BufferedReader var25 = new BufferedReader(new FileReader(tempFile));
-            String var24 = "\n" + var25.readLine();
-            fops1.write(var24.getBytes());
-            fops1.close();
-            fops2.close();
-            var25.close();
-            tempFile.delete();
         }
 
         br.close();
